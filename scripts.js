01896a09d10f504e39f542641e3765f09cf9421f
ChernikM <chernikmaksim1992@gmail.com>
2024-04-09 23:39:49 +0300

    1

--- scripts.js
+++ scripts.js
@@ -42,7 +42,7 @@ async function fetchSessions() {
     },
   });
   const data = await response.json();
-  const content = atob(data.content);
+  const content = decodeURIComponent(escape(window.atob(data.content)));;
   return JSON.parse(content);
 }
 
